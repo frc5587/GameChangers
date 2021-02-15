9b174f22d0fb1895cd941df1d9b6826d74361f39
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2021-02-15 13:51:13 -0500

    commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,6 +29,7 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+    
   }
 
   /**
