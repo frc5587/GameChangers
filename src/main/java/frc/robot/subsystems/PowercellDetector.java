45527e6a652854a04b140f16d788347d6ab103cd
Johnny Wobble <46848052+JohnnyWobble@users.noreply.github.com>
2021-03-19 01:28:17 -0400

    Added x and y to PowercellDetector

--- src/main/java/frc/robot/subsystems/PowercellDetector.java
+++ src/main/java/frc/robot/subsystems/PowercellDetector.java
@@ -5,16 +5,16 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import Networ;/
 
 public class PowercellDetector extends SubsystemBase {
     private NetworkTable detectorTable = NetworkTableInstance.getDefault().getTable("powercellDetector");
     private NetworkTableEntry tn = detectorTable.getEntry("tn");
     private NetworkTableEntry tr = detectorTable.getEntry("tr");
-    private NetworkTableEntry tTheta = detectorTable.getEntry("tTheta");
+    private NetworkTableEntry tx = detectorTable.getEntry("tx");
+    private NetworkTableEntry ty = detectorTable.getEntry("ty");
+    private NetworkTableEntry ttheta = detectorTable.getEntry("ttheta");
 
     public PowercellDetector() {
-        
     }
 
     /**
@@ -36,7 +36,7 @@ public class PowercellDetector extends SubsystemBase {
     }
 
     /**
-     * Gets the distance of the closest powercell. 
+     * Gets the distance of the closest powercell.
      * 
      * NOTE: if it doesn't see any powercells, this will return 0
      * 
@@ -47,20 +47,50 @@ public class PowercellDetector extends SubsystemBase {
     }
 
     /**
-     * Gets the angle the closest powercell makes with the camera. Negative is to the left and positive is to the right
+     * Gets the angle the closest powercell makes with the camera. Negative is to
+     * the left and positive is to the right
+     * 
+     * NOTE: if it doesn't see any powercells, this will return 0
      * 
      * @return angle of powercell - DEGREES
      */
     public double getHorizontalAngleDegrees() {
-        return tTheta.getDouble(0);
+        return ttheta.getDouble(0);
     }
 
     /**
-     * Gets the angle the closest powercell makes with the camera. Negative is to the left and positive is to the right
+     * Gets the angle the closest powercell makes with the camera. Negative is to
+     * the left and positive is to the right
+     * 
+     * NOTE: if it doesn't see any powercells, this will return 0
      * 
      * @return angle of powercell - RADIANS
      */
     public double getHorizontalAngleRadians() {
         return Units.degreesToRadians(getHorizontalAngleDegrees());
+    }
+
+    /**
+     * Gets the X value of the position of the powercell assuming the camera is at
+     * (0, 0) and facing positive X, meaning positive Y is to it's right
+     * 
+     * NOTE: if it doesn't see any powercells, this will return 0
+     * 
+     * @return X value - METERS
+     */
+    public double getPowercellX() {
+        return tx.getDouble(0);
+    }
+
+    /**
+     * Gets the Y value of the position of the powercell assuming the camera is at
+     * (0, 0) and facing positive X, meaning positive Y is to it's right
+     * 
+     * NOTE: if it doesn't see any powercells, this will return 0
+     * 
+     * @return Y value - METERS
+     */
+    public double getPowercellY() {
+        return ty.getDouble(0);
     }
 }
